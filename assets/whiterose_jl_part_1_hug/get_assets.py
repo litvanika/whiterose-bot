adeaacc8d7d7cc01d62021ee8aa50e22e1ec00a6
litvanika <74470263+litvanika@users.noreply.github.com>
2023-12-18 21:59:08 +0300

    Updated whiterose_jl_part_1_hug

--- assets/whiterose_jl_part_1_hug/get_assets.py
+++ assets/whiterose_jl_part_1_hug/get_assets.py
@@ -3,7 +3,9 @@ import os
 
 def get_assets(path: str) -> list[str]:
     assets = [
+        'whiterose_ruby_sparkling_eyes_glad_to_see_weiss.jpeg',
         'whiterose_justice_league_jump_hug.jpeg',
         'whiterose_justice_league_hug.jpeg',
+        'whiterose_jl_part_1_hug_weiss_with_closed_eyes.jpeg',
     ]
     return [os.path.join(path, asset) for asset in assets]
